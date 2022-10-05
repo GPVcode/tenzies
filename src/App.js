96e12961fb0fac27a1d2c58cf3f4cc2ad5a6a00b
GPV <gvill005@gmail.com>
2022-10-04 21:23:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
+import React, { useState } from "react";
+import Die from "./components/Die"
+import {nanoid} from "nanoid"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  //set state variables, set new dice object as default state
+  const [ dice, setDice ] = useState(allNewDice)
+
+  //create function that generates random numbers
+  function allNewDice(){
+    const newDice = []
+    for(let i = 0; i < 10; i++){
+      const randomNumber = Math.floor(Math.random() * 6 ) + 1;
+      newDice.push({
+        value: randomNumber,
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+
+  const rollDice = () => {
+    setDice(allNewDice)
+  }
+
+  return(
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button onClick={rollDice} className="roll-dice">
+        Roll
+      </button>
+    </main>
+  )
 }
 
-export default App;
+
+export default App
