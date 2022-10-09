e86e34af9378d4d605157ae6a54b3daa8ea82f6a
GPV <gvill005@gmail.com>
2022-10-08 22:48:52 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -1,44 +1,69 @@
 import "./style.css"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react"
 import Die from "./components/Die"
-import {nanoid} from "nanoid"
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [ dice, setDice ] = useState(generateNewDice)
+  const [ tenzies, setTenzies ] = useState(false)
 
-  //set state variables, set new dice object as default state
-  const [ dice, setDice ] = useState(allNewDice)
-
-  //create function that generates random numbers
-  function allNewDice(){
-    const newDice = []
+  useEffect(() => {
+    const allHeldTrue = dice.every(die => die.isHeld === true)
+    const firstValue = dice[0].value;
+    const allValuesEqual = dice.every(die => die.value === firstValue)
+    if(allHeldTrue && allValuesEqual){
+      setTenzies(true)
+      tenzies && alert("You Win!")
+    }
+  }, [dice])
+  
+  function generateNewDice(){
+    const newDice = [];
     for(let i = 0; i < 10; i++){
-      const randomNumber = Math.floor(Math.random() * 6 ) + 1;
-      newDice.push({
-        value: randomNumber,
-        isHeld: true,
-        id: nanoid()
-      })
+      newDice.push(newDieGenerator())
     }
     return newDice
-  }
+  };
 
+  function newDieGenerator(){
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    return {
+      value: randomNumber,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
   function holdDice(id){
-    console.log(id)
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+      {...die, isHeld: !die.isHeld} : die
+    }))
   }
 
+  const diceElements = dice.map(die => 
+    <Die 
+      value={die.value} 
+      isHeld={die.isHeld} 
+      holdDice={() => holdDice(die.id)}
+    />
+  )
 
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={holdDice}/>)
-
-  const rollDice = () => {
-    setDice(allNewDice)
-  }
+  const rollDice = (id) => {
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld === true ? die : newDieGenerator()
+    }))
+}
 
   return(
     <main>
+      <h1 className="title">Tenzies!</h1>
+      <h3 className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+     
+      <button onClick={rollDice} className="roll-dice">Roll Dice</button>
+
     </main>
   )
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -5,8 +5,7 @@ function Die(props){
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
-
-
+  
 
     return (
         <div className="die-face" style={styles} onClick={props.holdDice}>
